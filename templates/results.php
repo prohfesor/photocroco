83e93f3e3823dffe703332be8005afedeadc10ad
Pavel Dvoinos <prohfesor@gmail.com>
2012-12-23 00:16:24 +0300

    fix quick php tags in results

--- templates/results.php
+++ templates/results.php
@@ -1,9 +1,9 @@
 <?php include('_header.php'); ?>
 
-<h3>The winner is: <?= $game['winner'] ?></h3>
+<h3>The winner is: <?php echo $game['winner'] ?></h3>
 
 <?php foreach ($game['answers'] as $answer) : ?>
-<p class="text-info"><?= key($answer) ?>: <b><?= reset($answer) ?></b></p>
+<p class="text-info"><?php echo key($answer) ?>: <b><?php echo reset($answer) ?></b></p>
 <?php endforeach; ?>
 
 <?php include('_footer.php'); ?>
