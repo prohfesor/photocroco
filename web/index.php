7c11a4fe53ba1454ad2c53496531e2b5a6366b53
Viktor Dziuban <vsybrex@gmail.com>
2012-12-22 16:46:57 +0200

    start with game logics

--- web/index.php
+++ web/index.php
@@ -1,15 +1,42 @@
 <?php
+define('PATH', dirname(__DIR__));
+
 $loader = require '../vendor/autoload.php';
 $loader->add('Flyers', dirname(__DIR__).'/src');
 
-require "../src/Flyers/fastcomet/flyInit.php";
-$db = flyDb::getInstance();
-$db->connect( DB_HOST, DB_USER , DB_PASS , DB_NAME );
-
 $app = new \Slim\Slim();
+$game = new \Flyers\Croco\Game();
 
-$app->get('/hello/:name', function ($name) {
-    $a = new \Flyers\Croco\Game();
+// Index
+$app->get('/', function () use ($app) {
+
+});
+
+// Create new game
+$app->post('/new/:login/:question', function ($login, $question) use ($app, $game) {
+    $gameId = $game->create($login, $question);
+    echo json_encode(array('id' => $gameId));
+});
+
+// Get/join game
+$app->post('/game/:id/:login', function ($id, $login) use ($app, $game) {
+    try {
+        $game->join($id, $login);
+        $status = $game->status($id);
+        echo json_encode($status);
+    } catch (Exception $e) {
+        echo json_encode('error');
+    }
+});
+
+// Get game status
+$app->get('/game/:id/status', function ($id) use ($app) {
+
+});
+
+// Answer
+$app->post('/game/:id/answer/:answer', function ($id, $answer) use ($app) {
+
 });
 
 $app->run();
